9c19802dcae5772e9f41f8dab8028a5e1a72ac96
Kim <rodrigkim@gmail.com>
2022-10-15 19:29:52 -0400

    commit tree ce25705496f4af1993de8c9d72594a6d11c56eb2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+
+  def initialize(sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def kind
+    if @sideA > 0 && @sideB > 0 && @sideC > 0 && @sideA + @sideB > @sideC && @sideB + @sideC > @sideA && @sideA + @sideC > @sideB 
+      if @sideA == @sideB 
+        if @sideA == @sideC
+          kind = :equilateral
+        else
+          kind = :isosceles
+        end
+      elsif @sideA == @sideC || @sideB == @sideC
+        kind = :isosceles
+      else
+        kind = :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
